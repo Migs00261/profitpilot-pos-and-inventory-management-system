71c27df2a57c146d3d009ed377bfd3c82a50776d
xyvie lyons <xyvielyons@gmail.com>
2024-06-09 02:12:57 +0300

    skeleton update

--- app/(POS)/inventory/brand/loading.tsx
+++ app/(POS)/inventory/brand/loading.tsx
@@ -2,23 +2,23 @@ import React from 'react'
 import { Skeleton } from '@nextui-org/react'
 function Loading() {
   return (
-    <div className='space-y-4'>
-        <div className="">
+    <div className='space-y-4 m-4'>
+        <div className="border-2 border-white rounded">
             <Skeleton>
                 <div className="w-full h-[50px] bg-default-300 rounded"></div>
             </Skeleton>
         </div>
-        <div className="">
+        <div className="border-2 border-white rounded">
             <Skeleton>
                 <div className="w-full h-[50px] bg-default-300 rounded"></div>
             </Skeleton>
         </div>
-        <div className="">
+        <div className="border-2 border-white rounded">
             <Skeleton>
                 <div className="w-full h-[50px] bg-default-300 rounded"></div>
             </Skeleton>
         </div>
-        <div className="">
+        <div className="border-2 border-white rounded">
             <Skeleton>
                 <div className="w-full h-screen bg-default-300 rounded"></div>
             </Skeleton>
