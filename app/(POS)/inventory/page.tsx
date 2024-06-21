aec352d2b8f1334efddf4ea0878e16474de6c746
xyvie lyons <xyvielyons@gmail.com>
2024-06-22 00:55:06 +0300

    loading skeletons for inventory

--- app/(POS)/inventory/page.tsx
+++ app/(POS)/inventory/page.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { useRouter } from 'next/navigation'
 import InventoryNavbarComponent from '../_components/InventoryNavbarComponent'
 import {Button} from "@nextui-org/react";
-import Link from 'next/link';
 export default function page() {
   const router = useRouter()
   return (
--- app/(POS)/inventory/warehouse/loading.tsx
+++ app/(POS)/inventory/warehouse/loading.tsx
@@ -2,25 +2,25 @@ import React from 'react'
 import { Skeleton } from '@nextui-org/react'
 function Loading() {
   return (
-    <div className='space-y-4 m-4'>
-        <div className="border-2 border-white rounded">
+    <div className='space-y-4 m-4 bg-white'>
+        <div className="">
             <Skeleton>
-                <div className="w-full h-[50px] bg-default-300 rounded"></div>
+                <div className="w-full h-[50px] bg-default-200 rounded"></div>
             </Skeleton>
         </div>
-        <div className="border-2 border-white rounded">
+        <div className="">
             <Skeleton>
-                <div className="w-full h-[50px] bg-default-300 rounded"></div>
+                <div className="w-full h-[50px] bg-default-200 rounded"></div>
             </Skeleton>
         </div>
-        <div className="border-2 border-white rounded">
+        <div className="">
             <Skeleton>
-                <div className="w-full h-[50px] bg-default-300 rounded"></div>
+                <div className="w-full h-[50px] bg-default-200 rounded"></div>
             </Skeleton>
         </div>
-        <div className="border-2 border-white rounded">
+        <div className="">
             <Skeleton>
-                <div className="w-full h-screen bg-default-300 rounded"></div>
+                <div className="w-full h-screen bg-default-200 rounded"></div>
             </Skeleton>
         </div>
     </div>
