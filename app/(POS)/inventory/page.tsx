ef4fc9a237e6bf442cfbf2636368ede54f755bb0
xyvie lyons <xyvielyons@gmail.com>
2024-06-22 00:38:33 +0300

    navbar router.push implementation

--- app/(POS)/inventory/page.tsx
+++ app/(POS)/inventory/page.tsx
@@ -1,8 +1,11 @@
+'use client'
 import React from 'react'
+import { useRouter } from 'next/navigation'
 import InventoryNavbarComponent from '../_components/InventoryNavbarComponent'
 import {Button} from "@nextui-org/react";
 import Link from 'next/link';
 export default function page() {
+  const router = useRouter()
   return (
     <div className=''>
       <div className="">
@@ -11,51 +14,51 @@ export default function page() {
       <div className="grid grid-cols-2 grid-rows-3 gap-4 h-screen p-[16px]">
 
         <div className="w-full h-full">
-           <Link href='/inventory/allproducts'>
-              <Button className='w-full h-full bg-white text-xl hover:bg-custom-gradient hover:text-white font-bold text-coolGray600'>
+         
+              <Button onClick={()=>router.push('/inventory/allproducts')} className='w-full h-full bg-white text-xl hover:bg-custom-gradient hover:text-white font-bold text-coolGray600'>
               Manage Products
               </Button>
-           </Link>
+          
         </div>
 
         <div className="w-full h-full">
-          <Link href='/inventory/brand'>
-            <Button className='w-full h-full bg-white text-xl hover:bg-custom-gradient hover:text-white font-bold text-coolGray600' >
+          
+            <Button onClick={()=>router.push('/inventory/brand')} className='w-full h-full bg-white text-xl hover:bg-custom-gradient hover:text-white font-bold text-coolGray600' >
             Add/view Brands
             </Button>
-          </Link>
+         
         </div>
 
         <div className="w-full h-full">
-          <Link href='/inventory/category'>
-            <Button className='w-full h-full bg-white text-xl hover:bg-custom-gradient hover:text-white font-bold text-coolGray600'>
+        
+            <Button onClick={()=>router.push('/inventory/category')} className='w-full h-full bg-white text-xl hover:bg-custom-gradient hover:text-white font-bold text-coolGray600'>
             Add/View Categories
             </Button>
-          </Link>
+        
         </div>
 
         <div className="w-full h-full">
-          <Link href="/inventory/variants">
-            <Button className='w-full h-full bg-white text-xl hover:bg-custom-gradient hover:text-white font-bold text-coolGray600'>
+         
+            <Button onClick={()=>router.push("/inventory/variants")} className='w-full h-full bg-white text-xl hover:bg-custom-gradient hover:text-white font-bold text-coolGray600'>
             Add/View Variants
             </Button>
-          </Link>
+          
         </div>
 
         <div className="w-full h-full">
-          <Link href="/inventory/units">
-            <Button className='w-full h-full bg-white text-xl hover:bg-custom-gradient hover:text-white font-bold text-coolGray600'>
+          
+            <Button onClick={()=>router.push('/inventory/units')} className='w-full h-full bg-white text-xl hover:bg-custom-gradient hover:text-white font-bold text-coolGray600'>
             Add/View Units
             </Button>
-          </Link>
+       
         </div>
 
         <div className="w-full h-full">
-          <Link href="/inventory/warehouse">
-            <Button className='w-full h-full bg-white text-xl hover:bg-custom-gradient hover:text-white font-bold text-coolGray600'>
+      
+            <Button onClick={()=>router.push('/inventory/warehouse')} className='w-full h-full bg-white text-xl hover:bg-custom-gradient hover:text-white font-bold text-coolGray600'>
             Add/View Warehouses
             </Button>
-          </Link>
+        
         </div>
       
 
