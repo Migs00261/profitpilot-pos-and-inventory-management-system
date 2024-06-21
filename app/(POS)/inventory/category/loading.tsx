f9c30a948f47e1d87f416436690b99d1d3a629b2
xyvie lyons <xyvielyons@gmail.com>
2024-06-22 01:02:36 +0300

    skeleton shadcn integration in inventory loading states

--- app/(POS)/inventory/category/loading.tsx
+++ app/(POS)/inventory/category/loading.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import { Skeleton } from '@nextui-org/react'
+import { Skeleton } from "@/components/ui/skeleton"
 function Loading() {
   return (
-    <div className='space-y-4 m-4 bg-white'>
+    <div className='space-y-4 m-4 '>
         <div className="">
             <Skeleton>
-                <div className="w-full h-[50px] bg-default-200 rounded"></div>
+                <div className="w-full h-[100px] rounded"></div>
             </Skeleton>
         </div>
         <div className="">
             <Skeleton>
-                <div className="w-full h-[50px] bg-default-200 rounded"></div>
+                <div className="w-full h-[100px] rounded"></div>
             </Skeleton>
         </div>
         <div className="">
             <Skeleton>
-                <div className="w-full h-[50px] bg-default-200 rounded"></div>
+                <div className="w-full h-[100px] rounded"></div>
             </Skeleton>
         </div>
         <div className="">
             <Skeleton>
-                <div className="w-full h-screen bg-default-200 rounded"></div>
+                <div className="w-full h-screen rounded"></div>
             </Skeleton>
         </div>
     </div>
